8429b90d399d0a359794d675fae91a117d832a6a
Bill Leddy <wleddy@calpoly.edu>
2023-03-15 10:09:09 -0700

    Initial Commit

---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by Bill Leddy on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
